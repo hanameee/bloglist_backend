66de7773a6f8a38ca172e2e9e9c14f6da66092c9
hanameee <hanameee@gmail.com>
2020-03-06 02:23:06 +0900

    Replace requestLogger middleware to Morgan middleware

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ const express = require("express");
 const middleware = require("./utils/middleware");
 const bodyParser = require("body-parser");
 const notesRouter = require("./controllers/blogs");
-
+const morgan = require("morgan");
 const app = express();
 
 app.use(bodyParser.json());
-app.use(middleware.requestLogger);
-
+morgan.token("data", function(req, res) {
+    return JSON.stringify(req.body);
+});
+app.use(
+    morgan(
+        ":method :url :status :res[content-length] - :response-time ms :data"
+    )
+);
 app.get("/", (req, res) => {
     res.send("<h1>Hello World!</h1>");
 });
